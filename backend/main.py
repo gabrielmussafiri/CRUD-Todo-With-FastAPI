ef17fbee4d3a5d55bf73f17b976f034176d0f68c
gabrielmussafiri <musgabriel2@gmail.com>
2026-04-22 02:01:33 +0200

    Crud endpoints created

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,33 @@
 from fastapi import FastAPI
+from schemas.task import Task
 
 
 app = FastAPI()
 
 
+# Get All Tasks
 @app.get("/tasks")
 def get_taks():
-    return{"message":" All Tasks here"}
+    return{"message":" Show All Tasks here"}
 
+# Get One Task by Id
 @app.get("tasks/id")
 def get_task(id:int):
-    return{"message":f"get tasks by id {id}"}
+    return{"message":f"Show tasks by id {id}"}
+
+# create Tasks
+@app.post("/tasks")
+def create_task(t:Task):
+    return {"message":f"task {t} received"}
+
+# Update Tasks
+@app.put("/tasks/id")
+def update_task(id:int,t:Task):
+    return {"message":f"Taks {id} Updated  successfully , New task {t} "}
+
+
+# Delete Tasks
+@app.delete("/tasks/id")
+def delete_task(id:int):
+    
+    return {"message":f"Task {id} Deleted successfully"}
---
+++ backend/schemas/task.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+
+class Task(BaseModel):
+    title : str
+    description : str
+    stat : str
+    
